e3a23025c45bc8b88dbd39079a1036ea4ad08ab5
Mr <Ye@h>
2019-12-08 01:02:29 +0000

    Added more loadout combinations

--- gunfight.py
+++ gunfight.py
@@ -1,6 +1,5 @@
 import random
 import collections
-from numpy.random import choice as nchoice
 
 g_gear_itemsonly = "KLeMiNZacHjhIJkFfGl"
 
@@ -39,7 +38,7 @@ gear_type = collections.OrderedDict(sorted({
         "item":"UV",
         "primary":"KLeMiNZac",
         "secondary":"HjhIJk",
-        "sidearm":"FfGl"
+        "sidearm":"FfgGl"
 	}.items()))
 
 def gunfight_can_use_laser(loadout):
@@ -50,7 +49,7 @@ def gunfight_can_use_laser(loadout):
 
 def gunfight_can_use_silencer(loadout):
     for e in loadout:
-        if e in "FfghIJkLeMiNac":
+        if e in "FfghIJkLeMNac":
             return True
     return False
 
@@ -61,31 +60,44 @@ def gunfight_loadout_generate(loadouts=""):
 
         gearstring = list("AAAARWA")
 
-        loadout_type_weights = [0.005,0.005,0.265,0.39,0.335]
-        loadout_type = nchoice(gear_type.keys(),p=loadout_type_weights)
+        pick_primary = random.randint(0,1)
+        pick_secondary = random.randint(0,1)
+        pick_sidearm = pick_nade = pick_item = 0
+        if not (pick_primary and pick_secondary):
+            pick_sidearm = random.randint(0,1)
+            pick_nade = random.randint(0,1)
+            pick_item = random.randint(0,1) if not (pick_nade and (pick_primary or pick_secondary)) else 0
 
-        pick_nade = random.randint(0,1)
-        pick_item = random.randint(0,1)
-        if loadout_type in ("sidearm","primary","secondary"):
+	if pick_sidearm:
             gearstring[0] = random.choice(gear_type["sidearm"])
-        if loadout_type == "primary":
-            gearstring[1] = random.choice(gear_type["primary"])
-        if loadout_type == "secondary":
-            gearstring[2] = random.choice(gear_type["secondary"])
-        if loadout_type != "item":
-            if loadout_type == "grenade" or (pick_nade and (gearstring.count('A') != 2)):
-                gearstring[3] = random.choice(gear_type["grenade"])
-        if loadout_type not in ("grenade","item"):
-            if pick_item and (gearstring.count('A') != 2):
-                item = random.choice(gear_type["item"])
-                if (gunfight_can_use_laser(gearstring) and item == "V") or (gunfight_can_use_silencer(gearstring) and item == "U"):
-                    gearstring[6] = item
+	if pick_primary:
+            gearstring[1] = random.choice(gear_type["primary"]+gear_type["secondary"])
+	if pick_secondary:
+            gearstring[2] = random.choice(gear_type["secondary"]) if gearstring[1] is not 'c' else 'A'
+	if pick_nade:
+            gearstring[3] = random.choice(gear_type["grenade"])
+	if pick_item:
+            item = random.choice(gear_type["item"]) if not (pick_secondary and pick_primary) else 'A'
+            if (gunfight_can_use_laser(gearstring) and item == "V") or (gunfight_can_use_silencer(gearstring) and item == "U"):
+                gearstring[6] = item
+			
         return("".join(gearstring))
 
 def gunfight_print_loadout(gearstring,g_gear=""):
     textlist = []
-    if g_gear == g_gear_itemsonly:
-        gearstring = "AA"+gearstring[2:]
+
+    if gear_type["sidearm"] in g_gear:
+	 gl = list(gearstring)
+	 gl[0] = 'A'
+	 gearstring = "".join(gl)
+    if gear_type["primary"] in g_gear:
+	 gl = list(gearstring)
+	 gl[1] = 'A'
+	 gearstring = "".join(gl)
+    if gear_type["secondary"] in g_gear:
+	 gl = list(gearstring)
+	 gl[2] = 'A'
+	 gearstring = "".join(gl)
     if gearstring[0:4].count('A') >= 3:
         textlist.append("^1")
     if gearstring[0:4].count('A') == 4:
@@ -98,23 +110,41 @@ def gunfight_print_loadout(gearstring,g_gear=""):
     return(" ".join(textlist))
 
 def gunfight_next_loadout(spunky):
+    g_sidearm = gear_type["sidearm"]
+    g_primary = gear_type["primary"]
+    g_secondary = gear_type["secondary"]
+    g_gear = spunky.game.get_cvar('g_gear')
     # handling hot restart
-    if g_gear_itemsonly in spunky.game.get_cvar('g_gear'):
-	if not g_gear_itemsonly in spunky.default_gear:
-	    spunky.game.send_rcon("set g_gear \"\"")
-	    spunky.default_gear = ""
+    for g in (g_sidearm,g_primary,g_secondary):
+	    if g in g_gear: 
+                #if not g in spunky.default_gear:
+		    spunky.game.send_rcon("set g_gear \"\"")
+		    spunky.default_gear = ""
 
-    current_loadout = spunky.gunfight_round_loadout
-    while current_loadout == spunky.gunfight_round_loadout:
-        spunky.gunfight_round_loadout = gunfight_loadout_generate(spunky.gunfight_presets)
-    if spunky.gunfight_round_loadout[0:3] == "AAA":
-        gunfight_workaround = spunky.gunfight_round_loadout
-        spunky.game.send_rcon("set g_gear %s" % g_gear_itemsonly)
-        if spunky.gunfight_round_loadout[3] == "O":
-            gunfight_workaround = "GL" + spunky.gunfight_round_loadout[2:]
-        spunky.game.send_rcon("set sv_forcegear %s" % gunfight_workaround)
+    current_loadout = spunky.gunfight_loadout
+    while current_loadout == spunky.gunfight_loadout:
+        spunky.gunfight_loadout = gunfight_loadout_generate(spunky.gunfight_presets)
+
+    new_loadout = spunky.gunfight_loadout
+    restrictions = new_loadout[0:3].count('A')
+    new_g_gear = ""
+    if new_loadout[0:3].count('A'):
+        gunfight_workaround = new_loadout
+	if new_loadout[0] == 'A':
+	    new_g_gear += g_sidearm
+	    gunfight_workaround = "G" + new_loadout[1:]
+	    spunky.game.send_rcon("set g_gear %s" % new_g_gear)
+	if new_loadout[1] == 'A':
+	    new_g_gear += g_primary
+	if new_loadout[2] == 'A':
+	    new_g_gear += g_secondary
+	if restrictions == 3:
+		spunky.game.send_rcon("set g_gear %s" % g_gear_itemsonly)
+		if new_loadout[3] == "O":
+		    gunfight_workaround = "GL" + new_loadout[2:]
+	spunky.game.send_rcon("set sv_forcegear %s" % gunfight_workaround)
     else:
         if g_gear_itemsonly in spunky.game.get_cvar('g_gear'):
             spunky.game.send_rcon("set g_gear %s" % spunky.default_gear)
-        spunky.game.send_rcon("set sv_forcegear %s" % spunky.gunfight_round_loadout)
+        spunky.game.send_rcon("set sv_forcegear %s" % new_loadout)
 
